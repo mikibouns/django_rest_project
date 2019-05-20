c6e226e98b1b74f2f41e776d1ccbbb10433eaf76
mikibouns <mikibouns@gmail.com>
2019-05-20 16:55:39 +0200

    created products model

--- rest_project/product_app/models.py
+++ rest_project/product_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=256)
+    price = models.DecimalField(max_digits=2, default=0, max_length=10)
+    art = models.PositiveIntegerField(unique=True)
