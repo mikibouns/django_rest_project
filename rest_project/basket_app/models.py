ea4ddedbf661bc2d8f44c4461d77ad2c3409e872
mikibouns <mikibouns@gmail.com>
2019-05-25 00:07:06 +0200

    added new field 'quantity' in to basket model

--- rest_project/basket_app/models.py
+++ rest_project/basket_app/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 class Basket(models.Model):
     user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='user')
     product_list = models.ForeignKey(Products, on_delete=models.CASCADE, verbose_name='product_list')
+    quantity = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return str(self.id)
