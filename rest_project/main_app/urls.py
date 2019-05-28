71244eea5db4d9f6a331587c5d8acfcf19e533df
mikibouns <mikibouns@gmail.com>
2019-05-28 19:51:35 +0200

    fill_db correct

--- rest_project/main_app/urls.py
+++ rest_project/main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main_app'
 
 urlpatterns = [
-    path('', views.APIRootView.as_view()),
+    path('', views.APIRootView.as_view(), name='main_app'),
+    # path('api_doc', )
 ]
