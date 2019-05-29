1b2c65e264c650f4dc47fc89fcdc6dcb9e476157
mikibouns <mikibouns@gmail.com>
2019-05-29 09:07:31 +0200

    remove summernote

--- rest_project/rest_project/settings.py
+++ rest_project/rest_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_summernote',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
