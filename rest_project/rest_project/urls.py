cd2d5b39733f24f33483e7f9a78848b7574c4f44
mikibouns <mikibouns@gmail.com>
2019-05-29 10:54:28 +0200

    fix urls

--- rest_project/rest_project/urls.py
+++ rest_project/rest_project/urls.py
@@ -20,13 +20,11 @@ from django.conf.urls.static import static
 from rest_framework.authtoken import views as auth_token
 
 urlpatterns = [
-    path('api/v1/', include('main_app.urls', namespace='main_app')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    # path('schema/', schema_view, name='schema'),
-
     path('api-token-auth/', auth_token.obtain_auth_token, name='token_auth'),
 
+    path('api/v1/', include('main_app.urls', namespace='main_app')),
     path('api/v1/users/', include('auth_app.urls', namespace='auth')),
     path('api/v1/products/', include('product_app.urls', namespace='product')),
     path('api/v1/basket/', include('basket_app.urls', namespace='basket')),
