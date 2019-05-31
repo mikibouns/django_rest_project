7a2f65105204f360310aafc96e317fc066b801ce
mikibouns <mikibouns@gmail.com>
2019-05-31 18:17:39 +0200

    fix get_token

--- rest_project/rest_project/urls.py
+++ rest_project/rest_project/urls.py
@@ -8,8 +8,8 @@ from rest_framework.authtoken import views as auth_token
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    path('api-token-auth/', auth_token.obtain_auth_token, name='token_auth'),
 
+    path('api/v1/get-token/', auth_token.obtain_auth_token, name='get_token'),
     path('api/v1/', include('main_app.urls', namespace='main_app')),
     path('api/v1/users/', include('auth_app.urls', namespace='auth')),
     path('api/v1/products/', include('product_app.urls', namespace='product')),
