d2270aa5f44aa3163a1287079e55d274f3410a09
mikibouns <mikibouns@gmail.com>
2019-06-03 14:35:18 +0200

    added SwaggerSchemaView

--- rest_project/main_app/urls.py
+++ rest_project/main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework_swagger.views import get_swagger_view
 from . import views
+from rest_project.swagger_schema import SwaggerSchemaView
+
 
 schema_view = get_swagger_view(title='API')
 
@@ -9,5 +11,5 @@ app_name = 'main_app'
 
 urlpatterns = [
     path('', views.APIRootView.as_view(), name='main_app'),
-    path('swagger_api_docs/', schema_view, name='swagger_api_docs'),
+    path('swagger_api_docs/', SwaggerSchemaView.as_view(), name='swagger_api_docs'),
 ]
